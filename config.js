a3818381ac9fb65fe87590b7d971af6b6a1aa5d6
asembinov-cmyk <asembinov@gmail.com>
2026-07-21 17:52:56 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+window.CRM_CONFIG = {
+  url:  'https://ngudcdkijsintsofvsyh.supabase.co',
+  anon: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ndWRjZGtpanNpbnRzb2Z2c3loIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4NDYyODAzMiwiZXhwIjoyMTAwMjA0MDMyfQ.kZsLykLhC8VY44d1Zdr9hZooR-SO_O_uKuV4Zi5nQgg'
+};
